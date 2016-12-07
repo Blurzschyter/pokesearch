ab36ac9b4c8e66658a72f82760c0094a9d34764d
Blurzschyter <sharulnizar.razali@gmail.com>
2016-12-07 12:16:31 +0800

    added my current location

---
+++ PokeSearch/PokeSearch-Bridging-Header.h
@@ -0,0 +1,9 @@
+//
+//  PokeSearch-Bridging-Header.h
+//  PokeSearch
+//
+//  Created by NM on 07/12/2016.
+//  Copyright © 2016 nizar. All rights reserved.
+//
+
+#import "GeoFire.h"
--- PokeSearch/ViewController.swift
+++ PokeSearch/ViewController.swift
@@ -7,19 +7,88 @@
 //
 
 import UIKit
+import MapKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, MKMapViewDelegate, CLLocationManagerDelegate {
+    
+    @IBOutlet weak var mapView: MKMapView!
+    
+    let locationManager = CLLocationManager()
+    var mapHasCenteredOnce = false
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        mapView.delegate = self
+        mapView.userTrackingMode = MKUserTrackingMode.follow
+        
     }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        locationAuthStatus()
+    }
+    
+    func locationAuthStatus() {
+        if CLLocationManager.authorizationStatus() == .authorizedWhenInUse { //only authorize the location when the app in used. not always on the phone. to avoid battery drain
+            
+            mapView.showsUserLocation = true
+            
+        } else {
+            
+            locationManager.requestWhenInUseAuthorization()
+        }
+    }
+    
+    //didchangeauthorization or authorization
+    //this function called when user say yes or no during location permission request.
+    func locationManager(_ manager: CLLocationManager, didChangeAuthorization status: CLAuthorizationStatus) {
+        
+        if status == .authorizedWhenInUse {
+            
+            mapView.showsUserLocation = true
+        }
+    }
+    
+    func centerMapLocation(location: CLLocation) {
+        
+        let coordinateRegion = MKCoordinateRegionMakeWithDistance(location.coordinate, 2000, 2000)
+        
+        mapView.setRegion(coordinateRegion, animated: true)
+    }
+    
+    //userlocation didupdate
+    func mapView(_ mapView: MKMapView, didUpdate userLocation: MKUserLocation) {
+        
+        if let loc = userLocation.location {
+            if !mapHasCenteredOnce {
+                centerMapLocation(location: loc)
+                mapHasCenteredOnce = true
+            }
+        }
+    }
+    
+    //viewforannotation
+    func mapView(_ mapView: MKMapView, viewFor annotation: MKAnnotation) -> MKAnnotationView? {
+        
+        var annotationView: MKAnnotationView?
+        
+        if annotation.isKind(of: MKUserLocation.self) {
+            
+            annotationView = MKAnnotationView(annotation: annotation, reuseIdentifier: "User")
+            annotationView?.image = UIImage(named: "ash")
+        }
+        
+        return annotationView
+    }
+    
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func spotRandomPokemon(_ sender: AnyObject) {
+    }
 
 }
 
